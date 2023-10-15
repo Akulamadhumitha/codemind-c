782ffe0a789858a83b5145dc46741efbae307ca8
Akulamadhumitha <147419849+Akulamadhumitha@users.noreply.github.com>
2023-10-15 12:23:09 +0530

    #include<stdio.h> int main() {     int cp,sp;     scanf("%d%d",&cp,&sp);     printf("%s",cp>sp?"Loss":(cp<sp)?"Profit":"No Profit and No Loss");
    }

---
+++ Profit_or_Loss.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int cp,sp;
+    scanf("%d%d",&cp,&sp);
+    printf("%s",cp>sp?"Loss":(cp<sp)?"Profit":"No Profit and No Loss");
+    
+    
+    
+    
+    
+    
+    
+}
