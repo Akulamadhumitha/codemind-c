bf59a4783d89aac50066b93e42a4cf1e71fab792
Akulamadhumitha <147419849+Akulamadhumitha@users.noreply.github.com>
2023-10-15 10:56:28 +0530

    #include<stdio.h> int main() {    float a,b;    scanf("%f%f",&a,&b);    if(a/b>=0.2)    {        printf("YES");    }    else     {         printf("NO");     }
    }

---
+++ Reach_Home.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+{
+   float a,b;
+   scanf("%f%f",&a,&b);
+   if(a/b>=0.2)
+   {
+       printf("YES");
+   }
+   else
+    {
+        printf("NO");
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+}
