5119afe64fbc61e4233eeff107139c9434f993f9
Akulamadhumitha <147419849+Akulamadhumitha@users.noreply.github.com>
2023-12-19 11:41:24 +0530

    #include<stdio.h> int main() {     int i,n;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     int odd=0;     for(i=1;i<n;i++)     {         if(arr[i]%2!=0)         odd=i;     }     printf("%d",odd); }

---
+++ Last_odd_number_index_position.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int i,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int odd=0;
+    for(i=1;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        odd=i;
+    }
+    printf("%d",odd);
+}
